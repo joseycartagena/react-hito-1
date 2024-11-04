551bb02f64833ed74f619ef8125defa0fbdf5a43
joseycartagena <jose.cartagena.c@gmail.com>
2024-11-04 19:47:20 -0300

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/styles.css'
+import MyNavbar from './components/MyNavbar';
+import MyHome from './components/MyHome';
+import MyFooter from './components/MyFooter';
+
+function App() {
+
+  return (
+    <>
+      <MyNavbar />
+      <MyHome />
+      <MyFooter />
+    </>
+  )
+}
+
+export default App
